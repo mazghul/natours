f81472d1de1156f90c5e5d86604f7c24a8b7d4bc
Maz <mazghul@gmail.com>
2019-09-20 02:47:22 +0530

    Formatting files and adding eslint.

--- app.js
+++ app.js
@@ -1,80 +1,22 @@
-const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+//1) Middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
-
+//Router
 app.get('/', (req, res) => {
-    res.status(200).send('Hello from Node JS');
+  res.status(200).send('Hello from Node JS');
 });
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-app.get('/api/v1/tours', (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            tours
-        }
-    })
-    ;
-});
-
-app.get('/api/v1/tours/:id', (req, res) => {
-    const id = req.params.id * 1;
-    const tour = tours.find(el => el.id === id);
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour
-        }
-    })
-    ;
-});
-
-app.post('/api/v1/tours', (req, res) => {
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({id: newId}, req.body);
-    tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: newTour
-            }
-        });
-    });
-});
-
-app.patch('/api/v1/tours/:id', (req, res) => {
-    res.status(201).json({
-        status: 'success',
-        data: {
-            tour: '<Updated Tour>'
-        }
-    });
-    /*const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({id: newId}, req.body);
-    tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: newTour
-            }
-        });
-    });*/
-});
-
-app.delete('/api/v1/tours/:id', (req, res) => {
-    res.status(204).json({
-        status: 'success',
-        data: null
-    })
-})
-const port = 8080;
-app.listen(port, () => {
-    console.log(`Running on port ${port}..`);
-});
+module.exports = app;
