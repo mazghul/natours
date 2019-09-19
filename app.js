fb81463474d6528d1f7b6a67830a796f4fced132
Maz <mazghul@gmail.com>
2019-09-20 00:38:19 +0530

    Simple CRUD operations using express

---
+++ app.js
@@ -0,0 +1,80 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello from Node JS');
+});
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
+    ;
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    })
+    ;
+});
+
+app.post('/api/v1/tours', (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newId}, req.body);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        });
+    });
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+    res.status(201).json({
+        status: 'success',
+        data: {
+            tour: '<Updated Tour>'
+        }
+    });
+    /*const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newId}, req.body);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        });
+    });*/
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+    res.status(204).json({
+        status: 'success',
+        data: null
+    })
+})
+const port = 8080;
+app.listen(port, () => {
+    console.log(`Running on port ${port}..`);
+});
